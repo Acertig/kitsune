b8c1f23640637965996c679aa70dcd7f991d8e97
syster <67067768+Acertig@users.noreply.github.com>
2021-06-20 17:48:59 -0500

    Update doujin.py

--- doujin.py
+++ doujin.py
@@ -8,6 +8,7 @@ class Doujin:
         self.languages = kwargs.get("languages", None)
         self.uploaded = kwargs.get("uploaded", None)
         self.pages = kwargs.get("pages", None)
+        self.code = kwargs.get("code", None)
         self.content_cover = kwargs.get("content_cover", None)
         self.content_pages = kwargs.get("content_pages", None)
         self.content_recommended = kwargs.get("content_recommended", None)
