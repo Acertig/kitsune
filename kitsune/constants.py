b553e9896c735812994d6d521854c701607251ed
syster <67067768+Acertig@users.noreply.github.com>
2021-10-02 03:02:12 -0500

    Update constants.py

--- kitsune/constants.py
+++ kitsune/constants.py
@@ -1,3 +1,2 @@
-ROUTE = "https://nhentai.net"
-API_ENDPOINT = "/api/gallery/"
-DEFAULT_HEADERS = {'User-agent': 'Mozilla/5.0'}
+BASE = "http://nhentai.net"
+DEFAULT_HEADERS = {'User-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/93.0.961.38"}
