56d4d0f95bc4f9041c37249154968a4eb2b1607f
syster <67067768+Acertig@users.noreply.github.com>
2021-10-06 23:30:01 -0500

    Update routes.py

--- kitsune/routes.py
+++ kitsune/routes.py
@@ -1,59 +1,26 @@
 from enum import Enum
+from typing import Any
 
-from constants import BASE
-
-__all__ = ("Popularity", "Filtering",)
-
-class FieldsNotValid(Exception): 
-    pass
-
+__all__ = ("Popularity",)
 
 class Popularity(Enum): 
     RECENT = ""
     TODAY = "popular-today"
     WEEK = "popular-week"
+    MONTH = "popular-month"
+    YEAR = "popular-year"
     ALL_TIME = "popular"
 
-class Category(Enum): 
-    TAGS = "tags"
-    ARTISTS = "artists"
-    CHARACTERS = "characters"
-    PARODIES = "parodies"
-    GROUPS = "groups"
-    LANGUAGE = "language"
-
-# Main objects
-
-class APIRoute:
-
-    __slots__ = ("id",)
-
-    def __init__(self, id: int): 
-        self.id = id
+class Route: 
     
-    @property
-    def url(self): 
-        return BASE + f"/api/gallery/{self.id}"
+    BASE = "http://nhentai.net"
 
-class SearchRoute: 
-    
-    __slots__ = ("query", "popularity", "category",)
+    def __init__(self, path: str, *args): 
+        self.path = path
+        self.url = self.BASE + self.path
+        
+        for arg in args: 
+            self.url = self.url.replace("{}", str(arg), 1)
 
-    def __init__(self, query: str, popularity: Popularity, category: Category):
-        self.query = query
-        self.popularity = popularity
-        self.category = category
-
-        if self.popularity != Popularity.RECENT and not isinstance(self.popularity, Popularity): 
-            raise FieldsNotValid("Popularity field should be of type: enum Popularity.")
-
-        if self.category is not None and not isinstance(self.category, Category):  
-            raise FieldsNotValid("Category field should be of type: enum Category.") 
-
-    def get_url(self, page: int) -> str: 
-        if self.category is None: 
-            return BASE + f"/search/?q={self.query}&sort={self.popularity.value}&page={page}"
-
-        return BASE + f"/{self.category.value}/{self.query}/{self.popularity.value}?page={page}"
-
-
+    def insert_temporal_param(self, param: Any): 
+        return self.url.replace("{}", param)
