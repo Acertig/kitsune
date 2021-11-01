91cc7a07574ebe4393bc8a30b53cfd5b920d49ea
syster <67067768+Acertig@users.noreply.github.com>
2021-11-01 17:54:54 -0500

    Update http_handler.py

--- kitsune/http_handler.py
+++ kitsune/http_handler.py
@@ -1,3 +1,4 @@
+from collections.abc import Sequence
 from typing import List, Dict, Any, Union
 
 import re
@@ -5,8 +6,8 @@ import asyncio
 import aiohttp
 
 from kitsune.constants import TBR
-from kitsune.doujin import Page, Cover
-from kitsune.routes import Popularity, Route
+from kitsune.routes import Route
+from kitsune.query import Popularity
 
 class HTTPHandler:
 
@@ -23,12 +24,15 @@ class HTTPHandler:
             self = args[0]
             await self.lock.acquire()
             self.loop.call_later(TBR, self.lock.release)
-            return await async_func(*args, **kwargs)
+            result = await async_func(*args, **kwargs)
+            assert result is not None
+            return result
         return wrapper 
 
     @ratelimit
     async def get(self, route: Route, **params) -> Union[Dict[str, Any], str, bytes, None]: 
         async with self.session.get(route.url, params = params) as response: 
+            print(response.url)
             if 200 <= response.status < 300:    
                 content_type = response.headers["Content-Type"]
                 if content_type == "application/json": 
@@ -52,6 +56,7 @@ class HTTPHandler:
         html = await self.get(route)
 
         ids = re.findall(r"/g/(\d+)/", html)
+        
         filtered = [__id for __id in ids if ids.index(__id) != 0]
 
         return filtered
@@ -64,9 +69,17 @@ class HTTPHandler:
 
         return ids
 
+    async def fetch_paginator_limit(self, query: str, popularity: Popularity) -> int: 
+        route = Route(f"/api/galleries/search?query={query}")
+        payload = await self.get(route, page = 1, popularity = popularity.value)
+
+        limit = payload["num_pages"] if payload else 0
+
+        return limit
+
     async def fetch_search_data(self, query: str, page: int, popularity: Popularity) -> Dict[str, Any]:
-        route = Route(f"/api/galleries/search")     
-        payload = await self.get(route, query = query, page = page, popularity = popularity.value)
+        route = Route(f"/api/galleries/search?query={query}")     
+        payload = await self.get(route, page = page, popularity = popularity.value)
 
         return payload
 
@@ -76,20 +89,8 @@ class HTTPHandler:
 
         return payload
 
-    async def fetch_related_data(self, __id: int) -> List[int]: 
-        route = Route(f"/g/{__id}")
-        html = await self.get(route)
-
-        ids = re.findall(r"/g/(\d+)/", html)
-
-        filtered = [__id for __id in ids if ids.index(__id) != 0]
-
-        return filtered
-
-    async def fetch_media_bytes(self, media: List[Union[Page, Cover]]) -> List[bytes]: 
-        routes = [Route(media.url) for media in media]
+    async def fetch_media_bytes(self, media: Sequence[str]) -> List[bytes]: 
+        routes = [Route(media) for media in media]
         bytes_l = await asyncio.gather(*(self.get(route) for route in routes))
 
         return bytes_l
-
-        
